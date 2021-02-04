494b68f39994ca861b627f8a20ea0cf4182aa620
Guto Kuster <gutokuster@gmail.com>
2021-02-03 22:57:14 -0300

    Exemplo Binario

---
+++ ag_binario.r
@@ -0,0 +1,32 @@
+mochila <- data.frame(item = c("canivete", "feijão", "batatas", "lanterna",
+                               "saco de dormir", "corda", "bússola"), pontos = c(
+                                 10, 20, 15, 2, 30, 10, 30), peso = c(1, 5, 10, 
+                                                                      1, 7, 5 ,1))
+
+f <- function(x)
+{
+  pontos =0
+  peso = 0
+  for (i in 1:7)
+  {
+    if (x[i] != 0)
+    {
+      pontos = pontos + mochila[i,2]
+      peso = peso + mochila[i,3]
+    }
+  }
+if (peso > 15)
+  pontos = 0
+
+  return(pontos)
+}
+
+resultado = ga("binary", fitness = f, nBits = 7, popSize = 10, maxiter = 100, 
+               names = c("canivete", "feijão", "batatas", "lanterna",
+                         "saco de dormir", "corda", "bússola"))
+
+summary(resultado)
+
+summary(resultado)$solution
+
+plot(resultado)
